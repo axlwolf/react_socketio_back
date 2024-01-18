b23fd3cb4b87e587819f76c27e010c746cccc35b
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2024-01-18 13:00:20 -0600

    4. Front end finished

--- server/index.js
+++ server/index.js
@@ -6,7 +6,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
-io.on("connection", (socket) => console.log("client connected"));
+io.on("connection", (socket) => {
+	console.log("client connected");
+	console.log(socket.id);
+	socket.on("message", (body) => {
+		socket.broadcast.emit("message", {
+			body,
+			from: socket.id.slice(8),
+		});
+	});
+});
 
 server.listen(4000);
 console.log("server listening on port", 4000);
